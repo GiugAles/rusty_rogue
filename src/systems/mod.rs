926bfa7a90cc19dee70a2614d670b263aaf1cf0b
Wedigo <alessandro.giugno@gmx.de>
2022-01-22 17:18:51 +0100

    Adding system to render entities

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,5 +1,6 @@
 mod map_render;
 mod player_input;
+mod entity_render;
 
 use crate::prelude::*;
 
@@ -7,5 +8,6 @@ pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
         .build()
 }
